1720066be571c90a4286df41599ea3f16151e9e4
AndresiniGiovanni <gio.andresini@hotmail.it>
2022-10-06 15:30:02 +0200

    struttura js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,24 @@
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
+let percorso = parseInt(prompt("quanti km vuoi percorrere ?"));
+let eta = parseInt(prompt("qual'é l'etá del passeggero?"));
+
+console.log(percorso);
+console.log(eta);
+
+let costoKm = parseFloat(percorso * 0.21).toFixed(2);
+console.log(costoKm);
+
+if (eta < 18) {
+  let percentuale = parseFloat(costoKm * 0.8).toFixed(2);
+  console.log(percentuale);
+} else if (eta > 65) {
+  let percentuale = parseFloat(costoKm * 0.6).toFixed(2);
+  console.log(percentuale);
+} else {
+  let percentuale = costoKm;
+  console.log(percentuale);
+}
